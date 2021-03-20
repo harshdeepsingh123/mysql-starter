c6301de078e1de7748f71a45503abb4e98a0d62d
harshdeepsingh <harshwaliahsw@gmail.com>
2021-03-20 16:47:21 +0530

    sql

--- starter.sql
+++ starter.sql
@@ -1,23 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `employeaddoredit`(
-in _employeid varchar(10),
-in _name varchar(20),
-in _rollno varchar(10),
-in _employeecode varchar(20)
-)
-BEGIN
-	if _employeid = 0 then 
-		insert into employee(name,rollno,employeecode)
-        values(_name,_rollno,_employeecode);
-        
-        set _employeid=last_insert_id();
-	else
-		update employee
-        set 
-        name=_name,
-        rollno=_rollno,
-        employeecode=_employeecode
-        where employeid=_employeid;
-	end if;
-    
-	select _employeid as 'employeid';
-END
+use employedb;
+create table employee(employeid varchar(10),name varchar(20),rollno varchar(10),employeecode varchar(20));
+insert into employee values(1,'harsh',9,'emp90'),(2,'singh',10,'emp91');
+
